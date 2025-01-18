0854cdc1902b5ed057f7614159fee45f0af4d028
harischaerisma <junko.ghc37@gmail.com>
2025-01-18 16:33:21 +0700

    data baru create accoung

--- cypress/e2e/magneto/magneto.cy.js
+++ cypress/e2e/magneto/magneto.cy.js
@@ -1,7 +1,13 @@
 describe('template spec', () => {
     it('passes', () => {
       cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
+      cy.get('#firstname').type ('haris')
+      cy.get('#lastname').type ('chaerisma')
+      cy.get('#email_address').type ('haris.chaerismaaa@gmail.com')
+      cy.get('#password').type ('Ubiungu12345678')
+      cy.get('#password-confirmation').type ('Ubiungu12345678')
+      cy.get('button').contains('Create an Account').click()
     })
 
-      })
+      
   })
